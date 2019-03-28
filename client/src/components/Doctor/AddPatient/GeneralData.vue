f6d57a3c3027355275e7553247caca475401cead
SolyaHetman <solyahteman2013@gmail.com>
2019-03-28 10:58:41 +0200

    add sidebar navigation

---
+++ client/src/components/Doctor/AddPatient/GeneralData.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-tabs
+        v-model="active"
+        color="primary"
+        dark
+        slider-color="yellow"
+        >
+          <v-tab
+            v-for="n in tabs"
+            :key="n"
+            ripple
+          >
+          {{ n.title }}
+          </v-tab>
+          <v-tab-item
+            v-for="n in tabs"
+            :key="n"
+          >
+            <v-card flat>
+              <v-card-text>
+                {{ n.body }}
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+        </v-tabs>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'GeneralData',
+    data() {
+      return {
+        tabs: [
+          {
+            title:'Загальні дані',
+            body: "some lorem text1"
+          },
+          {
+            title:'Діагноз первинного імунодефіциту',
+            body: "some lorem text2"
+          },
+          {
+            title:'Стовбурові клітини',
+            body: "some lorem text3"
+          },
+          {
+            title:'Замісна імуноглобулінотерапія',
+            body: "some lorem text4"
+          }
+        ]
+      }
+    }
+  }
+</script>
--- client/src/views/Doctor/NewPatient.vue
+++ client/src/views/Doctor/NewPatient.vue
@@ -1,3 +1,17 @@
 <template>
-    <h1>This is new patient</h1>
-</templete>
+  <section class="new-patient">
+    <GeneralData />
+  </section>
+</template>
+
+
+<script>
+import GeneralData from "@/components/Doctor/AddPatient/GeneralData.vue"
+
+export default {
+components: {
+    GeneralData
+  }
+};
+</script>
+
