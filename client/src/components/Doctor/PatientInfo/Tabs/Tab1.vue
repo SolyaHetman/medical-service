618f811d8517f1bfeb68ff9895d4aa6dbac69be3
AndriyStasyuk <36013142+AndriyStasyuk@users.noreply.github.com>
2019-04-10 00:02:06 +0300

    Update Tab1.vue

--- client/src/components/Doctor/PatientInfo/Tabs/Tab1.vue
+++ client/src/components/Doctor/PatientInfo/Tabs/Tab1.vue
@@ -1,3 +1,91 @@
 <template>
-    <h1>This is template 1</h1>
+  <v-form>
+    <v-container>
+      <v-layout>
+
+        <v-flex md2 >
+          <p>Ім'я</p>
+          <v-text-field
+            v-model="generalData.lastname"
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex md2>
+          <p>Прізвище</p>
+          <v-text-field
+            v-model="generalData.firstname"
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex md2>
+          <p>Побатькові</p>
+          <v-text-field
+            v-model="generalData.middlename"
+          ></v-text-field>
+        </v-flex>
+
+      </v-layout>
+    </v-container>
+
+
+    <v-container>
+      <v-layout>
+    	<v-flex md2>
+          <p>Дата народження</p>
+          <v-text-field
+            v-model="generalData.birthdate"
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex md2>
+          <p>Стать</p>
+          <v-text-field
+            v-model="generalData.Sex"
+          ></v-text-field>
+        </v-flex>
+
+      </v-layout>
+    </v-container>
+
+
+    <v-container>
+      <v-layout>
+        <v-flex md2 >
+          <p>Країна народження</p>
+          <v-text-field
+            v-model="generalData.Country"
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex md2>
+          <p>Країна теперішнього проживання</p>
+          <v-text-field
+            v-model="generalData.CountryN"
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+ </v-form>
 </template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        generalData: {
+          firstname: "Тарас",
+          lastname: "Кулаков",
+          middlename: "Миколайович",
+          birthdate: "18-03-2000",
+          Sex: "Чоловік",
+     	  Country: "Білорусь",
+     	  CountryN: "Україна",
+          numberESID:null,
+        } 
+      }
+    }
+  }
+</script>
+
