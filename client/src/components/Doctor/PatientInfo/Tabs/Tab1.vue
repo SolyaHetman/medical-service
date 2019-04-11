a89729046d088f2f96d7f691aacf657aa0aabddc
AndriyStasyuk <36013142+AndriyStasyuk@users.noreply.github.com>
2019-04-11 22:35:18 +0300

    Add page with general information

--- client/src/components/Doctor/PatientInfo/Tabs/Tab1.vue
+++ client/src/components/Doctor/PatientInfo/Tabs/Tab1.vue
@@ -1,44 +1,41 @@
 <template>
-  <v-form>
+  <v-form v-model="valid" ref="form">
     <v-container>
       <v-layout>
 
         <v-flex md2 >
-          <p>Ім'я</p>
+          <p><font color="#808080">Ім'я</font></p>
           <v-text-field
             v-model="generalData.lastname"
+            :rules="nameRules"
           ></v-text-field>
         </v-flex>
 
         <v-flex md2>
-          <p>Прізвище</p>
+          <p><font color="#808080">Прізвище</font></p>
           <v-text-field
             v-model="generalData.firstname"
+            :rules="nameRules"
           ></v-text-field>
         </v-flex>
 
         <v-flex md2>
-          <p>Побатькові</p>
+          <p><font color="#808080">Побатькові</font></p>
           <v-text-field
             v-model="generalData.middlename"
+            :rules="nameRules"
           ></v-text-field>
         </v-flex>
 
-      </v-layout>
-    </v-container>
-
-
-    <v-container>
-      <v-layout>
-    	<v-flex md2>
-          <p>Дата народження</p>
+        <v-flex md2>
+          <p><font color="#808080">Дата народження</font></p>
           <v-text-field
             v-model="generalData.birthdate"
           ></v-text-field>
         </v-flex>
 
         <v-flex md2>
-          <p>Стать</p>
+          <p><font color="#808080">Стать</font></p>
           <v-text-field
             v-model="generalData.Sex"
           ></v-text-field>
@@ -47,18 +44,17 @@
       </v-layout>
     </v-container>
 
-
     <v-container>
       <v-layout>
         <v-flex md2 >
-          <p>Країна народження</p>
+          <p><font color="#808080">Країна народження</font></p>
           <v-text-field
             v-model="generalData.Country"
           ></v-text-field>
         </v-flex>
 
         <v-flex md2>
-          <p>Країна теперішнього проживання</p>
+          <p><font color="#808080">Країна теперішнього проживання</font></p>
           <v-text-field
             v-model="generalData.CountryN"
           ></v-text-field>
@@ -66,26 +62,79 @@
       </v-layout>
     </v-container>
 
+    <v-container>
+      <v-layout>
+      <v-flex md3>
+          <p><font color="#808080">Згода пацієнта на використання особистої інформації</font></p>
+          <v-text-field
+            v-model="generalData.Agreement"
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex md2>
+          <p><font color="#808080">Родинні зв'язки ПІД</font></p>
+          <v-text-field
+            v-model="generalData.FamilyTies"
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex md2>
+          <p><font color="#808080">Спорідненість пацієнта</font></p>
+          <v-text-field
+            v-model="generalData.Affinity"
+          ></v-text-field>
+        </v-flex>
+
+      </v-layout>
+    </v-container>
+
+    <v-container>
+      <v-layout>
+        <v-flex md4>
+          <p><font color="#808080">Генетично ускладнений сімейний анамнез первинних імунодефіцитів</font></p>
+          <v-text-field
+            v-model="generalData.numberESID"
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-btn @click="submit">Зберегти</v-btn>
  </v-form>
 </template>
-
 
 <script>
   export default {
     data () {
       return {
+        valid: false,
+        date: null,
+        nameRules: [
+          (v) => !!v || 'Name is required',
+          (v) => v && v.length <= 20 || 'Name must be less than 20 characters'
+        ],
         generalData: {
-          firstname: "Тарас",
-          lastname: "Кулаков",
+          firstname:  "Тарас",
+          lastname:   "Кулаков",
           middlename: "Миколайович",
-          birthdate: "18-03-2000",
-          Sex: "Чоловік",
-     	  Country: "Білорусь",
-     	  CountryN: "Україна",
-          numberESID:null,
+          birthdate:  "18-03-2000",
+          Sex:        "Чоловік",
+     	    Country:    "Білорусь",
+     	    CountryN:   "Україна",
+          FamilyTies: "По чоловічій лінії",
+          Agreement:  "Згода лише на науковий аналіз",
+          Affinity:   "Невідомо",
+          numberESID: "Невідомо",
         } 
       }
-    }
+    },
+    methods: {
+      submit () {
+        this.$refs.form.validate()
+      },
+      save (date) {
+        this.$refs.menu.save(date)
+      },
+    },
   }
 </script>
 
