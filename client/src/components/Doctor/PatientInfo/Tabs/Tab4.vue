c9f7fed74529feff61806afa5964001257599497
AndriyStasyuk <36013142+AndriyStasyuk@users.noreply.github.com>
2019-04-12 00:28:44 +0300

    Add page with info about stem cells

--- client/src/components/Doctor/PatientInfo/Tabs/Tab4.vue
+++ client/src/components/Doctor/PatientInfo/Tabs/Tab4.vue
@@ -1,3 +1,65 @@
 <template>
-    <h1>This is template 4</h1>
+  <v-form v-model="valid" ref="form">
+    <v-container>
+      <v-layout>
+
+        <v-flex md4>
+          <p><font color="#808080">Чи будь-що з нижче наведеного було застосоване в даного пацієнта</font></p>
+          <v-text-field
+            v-model="stemCellsGeneticTheraty.stem_cells_transplantation"
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex md2 >
+          <p>
+            <font color="#808080">
+              Дата трансплантації
+            </font>
+          </p>
+          <v-text-field
+            v-model="stemCellsGeneticTheraty.transplantation_data"
+          ></v-text-field>
+        </v-flex>
+        <v-flex md2>
+          <p><font color="#808080">Джерело CD34 стовбурових  клітин</font></p>
+          <v-text-field
+            v-model="stemCellsGeneticTheraty.CD14_soure"
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex md2>
+          <p><font color="#808080">Генна терапія</font></p>
+          <v-text-field
+            v-model="stemCellsGeneticTheraty.genetic_therapy"
+          ></v-text-field>
+        </v-flex>
+
+      </v-layout>
+    </v-container>
+    <v-btn @click="submit">Зберегти</v-btn>
+ </v-form>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+            stemCellsGeneticTheraty:{
+                stem_cells_transplantation: null,
+                transplantation_data: "19-03-2007",
+                CD14_soure: "Кістковий мозок",
+                genetic_therapy: "Так",              
+            },
+            dataRules: [
+                  (v) => /^(\d{1,2})-(\d{1,2})-(\d{4})$/.test(v) || 'Введіть ДД-ММ-РР'
+                ],          
+        }
+    },
+    methods: {
+      submit () {
+        this.$refs.form()
+      },
+    }
+    
+}
+</script>
