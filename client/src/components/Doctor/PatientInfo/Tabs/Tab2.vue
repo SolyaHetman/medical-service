fba14757b9c8e47d10a1b623e3b98cfce96015b4
AndriyStasyuk <36013142+AndriyStasyuk@users.noreply.github.com>
2019-04-11 23:49:54 +0300

    Add page with path to diagnosis

--- client/src/components/Doctor/PatientInfo/Tabs/Tab2.vue
+++ client/src/components/Doctor/PatientInfo/Tabs/Tab2.vue
@@ -1,3 +1,101 @@
 <template>
-    <h1>This is template 2</h1>
+  <v-form v-model="valid" ref="form">
+    <v-container>
+      <v-layout>
+
+        <v-flex md2 >
+          <p><font color="#808080">Дата вперше встановленого клінічного діагнозу первинного імунодефіцину</font></p>
+          <v-text-field
+            v-model="pathDiagnosis.first_diagnostic_pid_dat"
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex md4 >
+          <p>
+          	<font color="#808080">
+          		Чи був встановлений діагноз ПІД без наявності ПІД-асоційованих симтомів і лише напідставі лабораторних відхилень
+          	</font>
+          </p>
+          <v-text-field
+            v-model="pathDiagnosis.pid_lab_only"
+          ></v-text-field>
+        </v-flex>
+        <v-flex md3>
+          <p><font color="#808080"><br/>Перші ПІД-асоційовані симтоми</font></p>
+          <v-text-field
+            v-model="pathDiagnosis.symptoms_start"
+          ></v-text-field>
+        </v-flex>
+
+      </v-layout>
+    </v-container>
+
+    <v-container>
+      <v-layout>
+      	<v-flex md2>
+          <p><font color="#808080">Початок симтомів</font></p>
+          <v-text-field
+            v-model="pathDiagnosis.symptoms_start_data"
+          ></v-text-field>
+        </v-flex>
+        <v-flex md2>
+          <p>
+          	<font color="#808080">
+          		IgG
+          	</font>
+          </p>
+          <v-text-field
+            v-model="pathDiagnosis.IgG"
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex md2>
+          <p><font color="#808080">IgA</font></p>
+          <v-text-field
+            v-model="pathDiagnosis.IgA"
+          ></v-text-field>
+        </v-flex>
+        <v-flex md2>
+          <p><font color="#808080">IgM</font></p>
+          <v-text-field
+            v-model="pathDiagnosis.IgM"
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-btn @click="submit">Зберегти</v-btn>
+ </v-form>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        valid: false,
+        date: null,
+        nameRules: [
+          (v) => !!v || 'Name is required',
+          (v) => v && v.length <= 20 || 'Name must be less than 20 characters'
+        ],
+        pathDiagnosis: {
+          first_diagnostic_pid_dat: "15-03-2005",
+          pid_lab_only: "Так, встановлено лімфопенію",
+          symptoms_start: "Інфекції, дисрегуляція іммуної відповіді",
+          symptoms_start_data: "27-03-2005",
+          IgG: "9.2 г/л",
+          IgA: "2 г/л",
+          IgM: "1.4 г/л",
+        } 
+      }
+    },
+    methods: {
+      submit () {
+        this.$refs.form.validate()
+      },
+      save (date) {
+        this.$refs.menu.save(date)
+      },
+    },
+  }
+</script>
+
