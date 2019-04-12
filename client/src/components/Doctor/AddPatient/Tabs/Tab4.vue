635a6f64cc85f2729025f5ef7555409c8cc17c03
SolyaHetman <solyahteman2013@gmail.com>
2019-04-12 09:15:30 +0300

    add change for   stemCellsGeneticTheraty

--- client/src/components/Doctor/AddPatient/Tabs/Tab4.vue
+++ client/src/components/Doctor/AddPatient/Tabs/Tab4.vue
@@ -4,16 +4,14 @@
       <v-layout>
 
         <v-flex xs12 md4>
-          <p>Чи будь-що з нижче наведеного було застосоване в даного пацієнта</p>   
+          <p>Чи була трасплатнація стовбурових клітин? </p>   
           <v-radio-group v-model=" stemCellsGeneticTheraty.stem_cells_transplantation" :mandatory="false">
-            <v-radio color="#1976d2" label="Трансплантація стурбурових клітин" value="no_mutations_detected"></v-radio>
             <v-radio color="#1976d2" label="Так" value="yes"></v-radio>
             <v-radio color="#1976d2" label="Ні" value="no"></v-radio>
             <v-radio color="#1976d2" label="Невідомо" value="unknown"></v-radio>
-            <v-text-field v-if="stemCellsGeneticTheraty.stem_cells_transplantation == 'yes'"
+            <v-text-field v-if="stemCellsGeneticTheraty.stem_cells_transplantation== 'yes'"
                 v-model= stemCellsGeneticTheraty.stem_cells_transplantation_yes
                 label="Введіть дані"
-                clearable
             ></v-text-field>
           </v-radio-group>
         </v-flex>
