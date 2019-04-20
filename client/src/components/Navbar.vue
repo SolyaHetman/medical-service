98fe09a1ab83516a399a54e9b1a3bce1ad349552
AndriyStasyuk <36013142+AndriyStasyuk@users.noreply.github.com>
2019-04-20 15:29:07 +0300

    Delete button Register

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -3,7 +3,6 @@
     <v-toolbar-title>MedicalService</v-toolbar-title>
     <v-toolbar-items class="hidden-sm-and-down ml-auto">
       <v-btn flat to="/login">Login</v-btn>
-      <v-btn flat>Register</v-btn>
       <v-btn flat to="/dashboard">Dashboard</v-btn>
       <v-btn flat to="/newpatient">Add New Patient</v-btn>
       <v-btn flat to="/patientinfo">Patient</v-btn>
