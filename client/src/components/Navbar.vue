4261305eef473d16fc72001c99dffef12a437676
AndriyStasyuk <andrii.stasiuk.o@gmail.com>
2019-09-11 18:38:46 +0300

    Add login form

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -2,6 +2,7 @@
   <v-toolbar class="primary" dark>
     <v-toolbar-title>MedicalService</v-toolbar-title>
     <v-toolbar-items class="hidden-sm-and-down ml-auto">
+      <v-btn flat to="/login">Логін</v-btn>  
       <v-btn flat to="/newpatient">Додати нового пацієнта</v-btn>  
       <v-btn flat to="/dashboard">Всі пацієнти</v-btn>
       <v-btn flat to="/doctorRegion">Лікар по області</v-btn>  
